849e4c6a38960a05a777b72017cc644e40682921
LarsTracy <larsatracy@gmail.com>
2024-02-07 22:03:05 -0700

    Return to monolithic cache validity

--- shared/src/main/java/chess/GameStateCache.java
+++ shared/src/main/java/chess/GameStateCache.java
@@ -3,9 +3,7 @@ package chess;
 import java.util.ArrayList;
 
 public class GameStateCache {
-    private boolean checkFlagsValid;
-    private boolean mateFlagsValid;
-    private boolean moveListsValid;
+    private boolean valid;
     private boolean checkFlagWhite;
     private boolean checkFlagBlack;
     private boolean checkmateFlagWhite;
@@ -14,60 +12,21 @@ public class GameStateCache {
     private boolean stalemateFlagBlack;
     private ArrayList<ChessMove> possibleMovesWhite;
     private ArrayList<ChessMove> possibleMovesBlack;
-    public enum cacheCategory {
-        CHECK_FLAGS,
-        MATE_FLAGS,
-        MOVE_LISTS,
-        ALL,
-    }
     public GameStateCache() {
-        checkFlagsValid = false;
-        mateFlagsValid = false;
-        moveListsValid = false;
+        valid = false;
     }
-    public boolean isValid(cacheCategory cacheCategory) {
-        switch (cacheCategory) {
-            case CHECK_FLAGS -> {
-                return checkFlagsValid;
-            } case MATE_FLAGS -> {
-                return mateFlagsValid;
-            } case MOVE_LISTS -> {
-                return moveListsValid;
-            // All
-            } default -> {
-                return checkFlagsValid && mateFlagsValid && moveListsValid;
-            }
-        }
+    public boolean isValid() {
+        return valid;
     }
-    public void invalidate(cacheCategory cacheCategory) {
-        switch (cacheCategory) {
-            case CHECK_FLAGS -> checkFlagsValid = false;
-            case MATE_FLAGS -> mateFlagsValid = false;
-            case MOVE_LISTS -> moveListsValid = false;
-            //ALL
-            default -> {
-                checkFlagsValid = false;
-                mateFlagsValid = false;
-                moveListsValid = false;
-            }
-        }
+    public void invalidate() {
+        valid = false;
     }
-    public void validate(cacheCategory cacheCategory) {
+    public void validate() {
         // Should I add checking here?
-        switch (cacheCategory) {
-            case CHECK_FLAGS -> checkFlagsValid = true;
-            case MATE_FLAGS -> mateFlagsValid = true;
-            case MOVE_LISTS -> moveListsValid = true;
-            //ALL
-            default -> {
-                checkFlagsValid = true;
-                mateFlagsValid = true;
-                moveListsValid = true;
-            }
-        }
+        valid = true;
     }
     public boolean checkFlag(ChessGame.TeamColor teamColor) {
-        if (isValid(cacheCategory.CHECK_FLAGS)) {
+        if (isValid()) {
             return teamColor == ChessGame.TeamColor.WHITE ? checkFlagWhite : checkFlagBlack;
         } else {
             throw new RuntimeException("InvalidCache");
@@ -81,7 +40,7 @@ public class GameStateCache {
         }
     }
     public boolean checkmateFlag(ChessGame.TeamColor teamColor) {
-        if (isValid(cacheCategory.MATE_FLAGS)) {
+        if (isValid()) {
             return teamColor == ChessGame.TeamColor.WHITE ? checkmateFlagWhite : checkmateFlagBlack;
         } else {
             throw new RuntimeException("InvalidCache");
@@ -96,7 +55,7 @@ public class GameStateCache {
     }
 
     public boolean stalemateFlag(ChessGame.TeamColor teamColor) {
-        if (isValid(cacheCategory.MATE_FLAGS)) {
+        if (isValid()) {
             return teamColor == ChessGame.TeamColor.WHITE ? stalemateFlagWhite : stalemateFlagBlack;
         } else {
             throw new RuntimeException("InvalidCache");
@@ -111,7 +70,7 @@ public class GameStateCache {
     }
 
     public ArrayList<ChessMove> getPossibleMoves(ChessGame.TeamColor teamColor) {
-        if (isValid(cacheCategory.MOVE_LISTS)) {
+        if (isValid()) {
             return teamColor == ChessGame.TeamColor.WHITE ? possibleMovesWhite : possibleMovesBlack;
         } else {
             throw new RuntimeException("InvalidCache");
